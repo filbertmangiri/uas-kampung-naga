b3612d2420adb4c366346e2a248a503bc335002d
Filbert Mangiri <filbert.mangiri@student.umn.ac.id>
2021-12-04 22:22:50 +0700

    Update home.php

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -19,45 +19,47 @@
 					pastinya lengkap.</p>
 			</div>
 
-			<div class="col-6">
-				<?php
+			<?php if (session('acc_logged_in') !== true) : ?>
+				<div class="col-6">
+					<?php
 
-				if (session('login_error_msg')) : ?>
-					<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-						<symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-							<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
-						</symbol>
-					</svg>
-
-					<div class="alert alert-danger alert-dismissible" role="alert">
-						<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
-							<use xlink:href="#exclamation-triangle-fill">
+					if (session('login_error_msg')) : ?>
+						<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+							<symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+								<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
+							</symbol>
 						</svg>
 
-						<?= session('login_error_msg'); ?>
+						<div class="alert alert-danger alert-dismissible" role="alert">
+							<svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
+								<use xlink:href="#exclamation-triangle-fill">
+							</svg>
 
-						<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-					</div>
-				<?php endif; ?>
+							<?= session('login_error_msg'); ?>
 
-				<form action="<?= base_url('login'); ?>" method="post">
-					<?= csrf_field(); ?>
+							<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+						</div>
+					<?php endif; ?>
 
-					<div class="form-floating mb-3">
-						<input type="text" name="email_username" class="form-control" placeholder=" " value="<?= (string) old('email_username'); ?>">
-						<label>Email atau username</label>
-					</div>
+					<form action="<?= base_url('login'); ?>" method="post">
+						<?= csrf_field(); ?>
 
-					<div class="form-floating mb-3">
-						<input type="password" name="password" class="form-control" placeholder=" ">
-						<label>Password</label>
-					</div>
+						<div class="form-floating mb-3">
+							<input type="text" name="email_username" class="form-control" placeholder=" " value="<?= (string) old('email_username'); ?>">
+							<label>Email atau username</label>
+						</div>
 
-					<button type="submit" class="btn btn-dark text-white">Login</button>
+						<div class="form-floating mb-3">
+							<input type="password" name="password" class="form-control" placeholder=" ">
+							<label>Password</label>
+						</div>
 
-					<p class="text-white" style="padding:10px 0 0 8px">Belum punya akun, <a href="#" class="link-light">Daftar</a> dulu yuk!</p>
-				</form>
-			</div>
+						<button type="submit" class="btn btn-dark text-white">Login</button>
+
+						<p class="text-white" style="padding:10px 0 0 8px">Belum punya akun, <a href="#" class="link-light">Daftar</a> dulu yuk!</p>
+					</form>
+				</div>
+			<?php endif; ?>
 		</div>
 
 		<div class="container text-white" style="padding-top: 100px">
